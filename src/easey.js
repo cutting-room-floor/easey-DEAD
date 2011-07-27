321e2d5c405fa11736cb2ea98de57668fc60ff34
Tom MacWright <macwright@gmail.com>
2011-07-27 12:32:40 -0400

    Simple easeIN

--- src/easey.js
+++ src/easey.js
@@ -1,4 +1,5 @@
 var easey = {};
+
 // Handle double clicks, that zoom the map in one zoom level.
 easey.DoubleClickHandler = function(map) {
     if (map !== undefined) {
@@ -16,6 +17,11 @@ easey.DoubleClickHandler.prototype = {
     doubleClickHandler: null,
 
     getDoubleClick: function() {
+        function easeIn(t) {
+            return t * t;
+        }
+
+
         // Ensure that this handler is attached once.
         if (!this.doubleClickHandler) {
             var theHandler = this;
@@ -23,15 +29,23 @@ easey.DoubleClickHandler.prototype = {
                 // Get the point on the map that was double-clicked
                 var point = com.modestmaps.getMousePoint(e, theHandler.map);
                 var n = 0;
+                var duration = 100;
 
+                var start = +new Date();
+                var zs = theHandler.map.getZoom();
+                var zc = theHandler.map.getCenter();
                 var zi = window.setInterval(function z() {
                     // use shift-double-click to zoom out
-                    theHandler.map.zoomByAbout(e.shiftKey ? -0.1 : 0.1, point);
-                    if (++n === 10) {
+                    var delta = +new Date() - start;
+                    if (delta > duration) {
                         window.clearInterval(zi);
                         theHandler.map.setZoom(Math.round(theHandler.map.getZoom()));
+                        return;
                     }
-                }, 5);
+                    var t = delta / duration;
+                    var za = zs + (easeIn(t) * (e.shiftKey ? -1 : 1));
+                    theHandler.map.setZoom(za);
+                }, 0);
 
                 return com.modestmaps.cancelEvent(e);
             };
