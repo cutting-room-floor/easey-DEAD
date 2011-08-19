df8c20d490c39d4814fce1a64d1d85dbd8f16968
Tom MacWright <macwright@gmail.com>
2011-08-19 18:24:05 -0400

    Totally wrecking this API 'for good'

--- src/easey.js
+++ src/easey.js
@@ -2,36 +2,63 @@
     var easey = {},
         i; // the interval!
 
-    function easeIn(t) { return t * t; }
-    function easeOut(t) { return Math.sin(t * Math.PI / 2); }
+    var easings = {
+        easeIn: function(t) { return t * t; },
+        easeOut: function(t) { return Math.sin(t * Math.PI / 2); },
+        linear: function(t) { return t; }
+    };
 
-    easey.slow = function(map, goal, duration) {
+    easey.cancel = function() {
+        if (i) { window.clearInterval(i); }
+    };
+
+    easey.slow = function(map, options) {
         if (i) { window.clearInterval(i); }
 
         var start = (+new Date()),
             startZoom = map.getZoom(),
-            startCenter = map.getCenter();
+            startCenter = map.getCenter(),
+            startCoordinate = map.coordinate.copy();
 
-        if (typeof goal === 'number') {
-            goal = { zoom: goal };
-        } else if (goal.lat && typeof goal.lat === 'number') {
-            goal = { pan: goal };
+        if (typeof options === 'number') {
+            options = { zoom: options };
+        } else if (options.lat && typeof options.lat === 'number') {
+            options = { location: options };
+        } else if (options.x && typeof options.x === 'number') {
+            options = { coordinate: map.pointCoordinate(options) };
         }
 
-        z = goal.zoom || startZoom;
-        to = goal.pan || startCenter;
+        if (options.point) {
+            options.coordinate = map.pointCoordinate(options.point);
+        }
+
+        z = options.zoom || startZoom;
+        time = options.time || 1000;
+        callback = options.callback || function() {};
+        ease = easings[options.ease] || easings.easeOut;
 
         i = window.setInterval(function() {
             // use shift-double-click to zoom out
             var delta = (+new Date()) - start;
-            if (delta > duration) {
+            if (delta > time) {
                 map.setZoom(z);
-                return window.clearInterval(i);
+                window.clearInterval(i);
+                return callback();
             }
 
-            var t = easeOut(delta / duration);
-            map.setCenterZoom(MM.Location.interpolate(startCenter, to, t),
-                z == startZoom ? z : (startZoom * (1 - t) + z * t));
+            var t = ease(delta / time);
+            var tz = (z == startZoom) ? z : (startZoom * (1 - t) + z * t);
+            if (options.location) {
+                map.setCenterZoom(MM.Location.interpolate(startCenter, options.location, t),
+                    tz);
+            } else if (options.coordinate) {
+                var a = startCoordinate.copy().zoomTo(tz);
+                var b = options.coordinate.copy().zoomTo(tz);
+                map.coordinate = new MM.Coordinate(
+                    (a.row * (1 - t)) +    (b.row * t),
+                    (a.column * (1 - t)) + (b.column * t),
+                    tz);
+            }
             map.draw();
         }, 0);
     };
@@ -188,7 +215,7 @@
 
             easey.slow(this.map, {
                 pan: pan
-            }, speed);
+            }, speed / 100);
             this.prevMouse = null;
             this.map.parent.style.cursor = '';
 
