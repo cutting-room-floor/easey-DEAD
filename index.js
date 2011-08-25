2d2a0895e4fd2fad273c7742b65d3ae90a1f60db
Tom MacWright <macwright@gmail.com>
2011-08-25 12:07:01 -0400

    Taller map.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $.domReady(function() {
         new wax.mm.connector({
             tiles: ['http://a.tiles.mapbox.com/mapbox/1.0.0/world-glass/{z}/{x}/{y}.png'],
             scheme: 'tms'
-        }), new mm.Point(600, 600), [
+        }), new mm.Point(600, 900), [
             new easey.DragHandler(),
             new easey.DoubleClickHandler(),
             new easey.MouseWheelHandler()
