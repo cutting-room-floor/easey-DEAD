293d89a3c9d11ad6b1860426acc4bf11244b637f
Tom MacWright <tom@macwright.org>
2012-01-11 14:54:56 -0500

    First chunk of v2 work. Fixes #7

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # easey
 
 Easey adds easing to Modest Maps, giving it a serious workout.
+
+## Changelog
+
+## 1.0.0
+
+* Changes easey interface to decouple input from
+  easing pattern.
+* Modest Maps handlers are now in `easey.handlers.js`,
+  which is dependent on `easey.js`.
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var map;
 
-$.domReady(function() {
-    var mm = com.modestmaps,
-    map = new com.modestmaps.Map('map',
-        new wax.mm.connector({
-            tiles: ['http://a.tiles.mapbox.com/mapbox/1.0.0/world-glass/{z}/{x}/{y}.png'],
-            scheme: 'tms'
-        }), new mm.Point(600, 900), [
-            new easey.DragHandler(),
-            new easey.DoubleClickHandler(),
-            new easey.MouseWheelHandler()
-        ]);
-    map.setCenterZoom(new com.modestmaps.Location(30, -90), 4);
+window.onload = function() {
+    var mm = com.modestmaps;
+    wax.tilejson('http://a.tiles.mapbox.com/v3/mapbox.world-bright.jsonp',
+        function(tj) {
+        map = new com.modestmaps.Map('map',
+            new wax.mm.connector(tj), null, [
+                new easey.DragHandler(),
+                new easey.DoubleClickHandler(),
+                new easey.MouseWheelHandler()
+            ]);
+        map.setCenterZoom(new com.modestmaps.Location(30, -90), 4);
+    });
 
     $('button').click(function(b) {
         eval($('pre', this.parentNode).text());
     });
-});
+};
