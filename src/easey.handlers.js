d679ecd8ec296038d3ab37b222a33ce54ef75e61
Tom MacWright <tom@macwright.org>
2012-04-13 13:00:17 -0400

    Update handlers to bleeding-edge mm

--- src/easey.handlers.js
+++ src/easey.handlers.js
@@ -1,14 +1,9 @@
 (function(context, MM) {
 
-    easey.TouchHandler = function(map, options) {
-        if (map) {
-            this.init(map, options);
-        }
-    };
+    easey.TouchHandler = function() {
+      var handler = {};
 
-    easey.TouchHandler.prototype = {
-
-      init: function(map, options) {
+      handler.add = function(map) {
             var prevT = 0,
                 acceleration = 25.0,
                 speed = null,
@@ -23,8 +18,6 @@
                 oldPoint = null,
                 lastMove = null,
                 lastPinchCenter = null;
-
-            options = options || {};
 
             function animate(t) {
                 var dir = { x: 0, y: 0 };
@@ -111,9 +104,6 @@
             MM.addEvent(map.parent, 'touchend',
                 touchEndMachine);
 
-            options = {};
-            options.snapToZoom = options.snapToZoom || true;
-
             prevT = new Date().getTime();
             speed = { x: 0, y: 0 };
             MM.getFrame(animate);
@@ -199,7 +189,7 @@
             // When a pinch event ends, round the zoom of the map.
             function onPinched(p) {
                 // TODO: easing
-                if (options.snapToZoom) {
+                if (true) {
                     var z = map.getZoom(), // current zoom
                         tz = Math.round(z);     // target zoom
                     map.zoomByAbout(tz - z, p);
@@ -267,105 +257,108 @@
         }
     };
 
-    // Handle double clicks, that zoom the map in one zoom level.
-    easey.DoubleClickHandler = function(map) {
-        if (map !== undefined) {
-            this.init(map);
-        }
-    };
+    easey.DoubleClickHandler = function() {
+        var handler = {},
+            map;
 
-    easey.DoubleClickHandler.prototype = {
-
-        init: function(map) {
-            this.map = map;
-            MM.addEvent(map.parent, 'dblclick', this.getDoubleClick());
-        },
-
-        doubleClickHandler: null,
-
-        getDoubleClick: function() {
-
+        function doubleClick(e) {
             // Ensure that this handler is attached once.
-            if (!this.doubleClickHandler) {
-                var theHandler = this;
-                this.doubleClickHandler = function(e) {
-                    var map = theHandler.map,
-                    z = map.getZoom() + (e.shiftKey ? -1 : 1);
+            // Get the point on the map that was double-clicked
+            var point = MM.getMousePoint(e, map);
+            z = map.getZoom() + (e.shiftKey ? -1 : 1);
+            // use shift-double-click to zoom out
+            easey().map(map)
+                .to(map.pointCoordinate(MM.getMousePoint(e, map)).zoomTo(z))
+                .path('about').time(100).run();
+            return MM.cancelEvent(e);
+        }
 
-                    easey.map(map)
-                        .to(map.pointCoordinate(MM.getMousePoint(e, map)).zoomTo(z))
-                        .path('about').time(100).run();
+        handler.add = function(x) {
+            map = x;
+            MM.addEvent(map.parent, 'dblclick', doubleClick);
+            return handler;
+        };
 
-                    return MM.cancelEvent(e);
-                };
+        handler.remove = function() {
+            MM.removeEvent(map.parent, 'dblclick', doubleClick);
+        };
+
+        return handler;
+    };
+
+    easey.MouseWheelHandler = function() {
+        var handler = {},
+            map,
+            _zoomDiv,
+            ea = easey(),
+            prevTime,
+            precise = false;
+
+        function mouseWheel(e) {
+            var delta = 0;
+            prevTime = prevTime || new Date().getTime();
+
+            try {
+                _zoomDiv.scrollTop = 1000;
+                _zoomDiv.dispatchEvent(e);
+                delta = 1000 - _zoomDiv.scrollTop;
+            } catch (error) {
+                delta = e.wheelDelta || (-e.detail * 5);
             }
-            return this.doubleClickHandler;
-        }
-    };
 
-    // A handler that allows mouse-wheel zooming - zooming in
-    // when page would scroll up, and out when the page would scroll down.
-    easey.MouseWheelHandler = function(map) {
-        if (map !== undefined) {
-            this.init(map);
-        }
-    };
+            // limit mousewheeling to once every 200ms
+            var timeSince = new Date().getTime() - prevTime;
+            var point = MM.getMousePoint(e, map);
 
-    easey.MouseWheelHandler.prototype = {
-
-        init: function(map) {
-            this.map = map;
-            MM.addEvent(map.parent, 'mousewheel', this.getMouseWheel());
-        },
-
-        mouseWheelHandler: null,
-
-        getMouseWheel: function() {
-            // Ensure that this handler is attached once.
-            if (!this.mouseWheelHandler) {
-                var theHandler = this;
-                var prevTime = new Date().getTime();
-                this.mouseWheelHandler = function(e) {
-
-                    var delta = 0;
-                    if (e.wheelDelta) {
-                        delta = e.wheelDelta;
-                    } else if (e.detail) {
-                        delta = -e.detail;
-                    }
-
-                    var timeSince = new Date().getTime() - prevTime;
-
-                    if (Math.abs(delta) > 0 && (timeSince > 50)) {
-
-                        if (!easey.running()) {
-                          var map = theHandler.map,
-                          point = MM.getMousePoint(e, map),
-                          z = map.getZoom();
-                          easey.map(map)
-                            .to(map.pointCoordinate(MM.getMousePoint(e, map)).zoomTo(
-                            z + (delta > 0 ? 1 : -1)
-                            ))
-                            .path('about').time(200).run();
-                        } else {
-                            easey.zoom(easey.to().zoom + (delta > 0 ? 1 : -1));
-                        }
-
-                        prevTime = new Date().getTime();
-                    }
-
-                    // Cancel the event so that the page doesn't scroll
-                    return MM.cancelEvent(e);
-                };
+            if (Math.abs(delta) > 0 && (timeSince > 200) && !precise) {
+                map.zoomByAbout(delta > 0 ? 1 : -1, point);
+                prevTime = new Date().getTime();
+            } else if (precise) {
+                map.zoomByAbout(delta * 0.001, point);
             }
-            return this.mouseWheelHandler;
+            if (!ea.running()) {
+              var point = MM.getMousePoint(e, map),
+                  z = map.getZoom();
+              ea.map(map)
+                .to(map.pointCoordinate(MM.getMousePoint(e, map)).zoomTo(z + (delta > 0 ? 1 : -1)))
+                .path('about').time(200).run();
+            } else {
+                ea.zoom(ea.to().zoom + (delta > 0 ? 1 : -1));
+            }
+
+            // Cancel the event so that the page doesn't scroll
+            return MM.cancelEvent(e);
         }
+
+        handler.add = function(x) {
+            map = x;
+            _zoomDiv = document.body.appendChild(document.createElement('div'));
+            _zoomDiv.style.cssText = 'visibility:hidden;top:0;height:0;width:0;overflow-y:scroll';
+            var innerDiv = _zoomDiv.appendChild(document.createElement('div'));
+            innerDiv.style.height = '2000px';
+            MM.addEvent(map.parent, 'mousewheel', mouseWheel);
+            return handler;
+        };
+
+        handler.precise = function(x) {
+            if (!arguments.length) return precise;
+            precise = x;
+            return handler;
+        };
+
+        handler.remove = function() {
+            MM.removeEvent(map.parent, 'mousewheel', mouseWheel);
+            _zoomDiv.parentNode.removeChild(_zoomDiv);
+        };
+
+        return handler;
     };
 
-    easey.DragHandler = function() { };
+    easey.DragHandler = function() {
+        var handler = {},
+            map;
 
-    easey.DragHandler.prototype = {
-        init: function(map) {
+        handler.add = function(map) {
             var prevT = 0,
                 speed = null,
                 drag = 0.25,
@@ -429,7 +422,9 @@
             prevT = new Date().getTime();
             speed = { x: 0, y: 0 };
             MM.getFrame(animate);
-        }
+        };
+
+        return handler;
     };
 
-})(this, com.modestmaps);
+})(this, MM);
--- src/easey.js
+++ src/easey.js
@@ -1,165 +1,170 @@
 (function(context, MM) {
-    var easey = {},
-        running = false,
-        abort = false; // killswitch for transitions
 
-    var easings = {
-        easeIn: function(t) { return t * t; },
-        easeOut: function(t) { return Math.sin(t * Math.PI / 2); },
-        linear: function(t) { return t; }
-    };
+    var easey = function() {
+        var easey = {},
+            running = false,
+            abort = false; // killswitch for transitions
 
-    // to is the singular coordinate that any transition is based off
-    // three dimensions:
-    //
-    // * to
-    // * time
-    // * path
-    var from, to, map;
+        var easings = {
+            easeIn: function(t) { return t * t; },
+            easeOut: function(t) { return Math.sin(t * Math.PI / 2); },
+            linear: function(t) { return t; }
+        };
 
-    easey.stop = function() {
-        abort = true;
-    };
+        // to is the singular coordinate that any transition is based off
+        // three dimensions:
+        //
+        // * to
+        // * time
+        // * path
+        var from, to, map;
 
-    easey.running = function() {
-        return running;
-    };
+        easey.stop = function() {
+            abort = true;
+        };
 
-    easey.point = function(x) {
-        to = map.pointCoordinate(x);
-        return easey;
-    };
+        easey.running = function() {
+            return running;
+        };
 
-    easey.zoom = function(x) {
-        to.zoomTo(x);
-        return easey;
-    };
+        easey.point = function(x) {
+            to = map.pointCoordinate(x);
+            return easey;
+        };
 
-    easey.location = function(x) {
-        to = map.locationCoordinate(x);
-        return easey;
-    };
+        easey.zoom = function(x) {
+            to.zoomTo(x);
+            return easey;
+        };
 
-    easey.time = function(x) {
-        time = x;
-        return easey;
-    };
+        easey.location = function(x) {
+            to = map.locationCoordinate(x);
+            return easey;
+        };
 
-    easey.from = function(x) {
-        if (!arguments.length) return from.copy();
-        from = x.copy();
-        return easey;
-    };
+        easey.time = function(x) {
+            time = x;
+            return easey;
+        };
 
-    easey.to = function(x) {
-        if (!arguments.length) return to.copy();
-        to = x.copy();
-        return easey;
-    };
+        easey.from = function(x) {
+            if (!arguments.length) return from.copy();
+            from = x.copy();
+            return easey;
+        };
 
-    easey.path = function(x) {
-        path = paths[x];
-        return easey;
-    };
+        easey.to = function(x) {
+            if (!arguments.length) return to.copy();
+            to = x.copy();
+            return easey;
+        };
 
-    easey.map = function(x) {
-        map = x;
-        from = map.coordinate.copy();
-        to = map.coordinate.copy();
-        return easey;
-    };
+        easey.path = function(x) {
+            path = paths[x];
+            return easey;
+        };
 
-    function interp(a, b, p) {
-        if (p === 0) return a;
-        if (p === 1) return b;
-        return a + ((b - a) * p);
-    }
+        easey.map = function(x) {
+            map = x;
+            from = map.coordinate.copy();
+            to = map.coordinate.copy();
+            return easey;
+        };
 
-    var paths = {};
-
-    // The screen path simply moves between
-    // coordinates in a non-geographical way
-    paths.screen = function(a, b, t) {
-        var zoom_lerp = interp(a.zoom, b.zoom, t);
-        var az = a.copy();
-        var bz = b.copy().zoomTo(a.zoom);
-        return (new MM.Coordinate(
-            interp(az.row, bz.row, t),
-            interp(az.column, bz.column, t),
-            az.zoom)).zoomTo(zoom_lerp);
-    };
-
-    function ptWithCoords(a, b) {
-        // distance from the center of the map
-        var point = new MM.Point(map.dimensions.x / 2, map.dimensions.y / 2);
-        point.x += map.tileSize.x * (b.column - a.column);
-        point.y += map.tileSize.y * (b.row - a.row);
-        return point;
-    }
-
-    // The screen path means that the b
-    // coordinate should maintain its point on screen
-    // throughout the transition, but the map
-    // should move to its zoom level
-    paths.about = function(a, b, t) {
-        var zoom_lerp = interp(a.zoom, b.zoom, t);
-
-        var bs = b.copy().zoomTo(a.zoom);
-        var az = a.copy().zoomTo(zoom_lerp);
-        var bz = b.copy().zoomTo(zoom_lerp);
-        var start = ptWithCoords(a, bs);
-        var end = ptWithCoords(az, bz);
-
-        az.column -= (start.x - end.x) / map.tileSize.x;
-        az.row -= (start.y - end.y) / map.tileSize.y;
-
-        return az;
-    };
-
-    var path = paths.screen;
-
-    easey.t = function(t) {
-        map.coordinate = path(from, to, t);
-        map.draw();
-        return easey;
-    };
-
-    easey.future = function(parts) {
-        var futures = [];
-        for (var t = 0; t < 1; t += (1 / parts)) {
-            futures.push(lerp(from, to, t));
+        function interp(a, b, p) {
+            if (p === 0) return a;
+            if (p === 1) return b;
+            return a + ((b - a) * p);
         }
-        return futures;
-    };
 
-    var fastFrame = function(callback) {
-        window.setTimeout(function () {
-            callback(+new Date());
-        }, 1);
-    };
+        var paths = {};
 
-    easey.run = function(callback) {
-        var start = (+new Date());
+        // The screen path simply moves between
+        // coordinates in a non-geographical way
+        paths.screen = function(a, b, t) {
+            var zoom_lerp = interp(a.zoom, b.zoom, t);
+            var az = a.copy();
+            var bz = b.copy().zoomTo(a.zoom);
+            return (new MM.Coordinate(
+                interp(az.row, bz.row, t),
+                interp(az.column, bz.column, t),
+                az.zoom)).zoomTo(zoom_lerp);
+        };
 
-        running = true;
+        function ptWithCoords(a, b) {
+            // distance from the center of the map
+            var point = new MM.Point(map.dimensions.x / 2, map.dimensions.y / 2);
+            point.x += map.tileSize.x * (b.column - a.column);
+            point.y += map.tileSize.y * (b.row - a.row);
+            return point;
+        }
 
-        function tick() {
-            var delta = (+new Date()) - start;
-            if (abort) {
-                return void (abort = running = false);
-            } else if (delta > time) {
-                running = false;
-                map.coordinate = path(from, to, 1);
-                map.draw();
-                if (callback) return callback(map, n);
-            } else {
-                map.coordinate = path(from, to, easings.easeIn(delta / time));
-                map.draw();
-                fastFrame(tick);
+        // The screen path means that the b
+        // coordinate should maintain its point on screen
+        // throughout the transition, but the map
+        // should move to its zoom level
+        paths.about = function(a, b, t) {
+            var zoom_lerp = interp(a.zoom, b.zoom, t);
+
+            var bs = b.copy().zoomTo(a.zoom);
+            var az = a.copy().zoomTo(zoom_lerp);
+            var bz = b.copy().zoomTo(zoom_lerp);
+            var start = ptWithCoords(a, bs);
+            var end = ptWithCoords(az, bz);
+
+            az.column -= (start.x - end.x) / map.tileSize.x;
+            az.row -= (start.y - end.y) / map.tileSize.y;
+
+            return az;
+        };
+
+        var path = paths.screen;
+
+        easey.t = function(t) {
+            map.coordinate = path(from, to, t);
+            map.draw();
+            return easey;
+        };
+
+        easey.future = function(parts) {
+            var futures = [];
+            for (var t = 0; t < 1; t += (1 / parts)) {
+                futures.push(lerp(from, to, t));
             }
-        }
+            return futures;
+        };
 
-        MM.getFrame(tick);
+        var fastFrame = function(callback) {
+            window.setTimeout(function () {
+                callback(+new Date());
+            }, 1);
+        };
+
+        easey.run = function(callback) {
+            var start = (+new Date());
+
+            running = true;
+
+            function tick() {
+                var delta = (+new Date()) - start;
+                if (abort) {
+                    return void (abort = running = false);
+                } else if (delta > time) {
+                    running = false;
+                    map.coordinate = path(from, to, 1);
+                    map.draw();
+                    if (callback) return callback(map, n);
+                } else {
+                    map.coordinate = path(from, to, easings.easeIn(delta / time));
+                    map.draw();
+                    fastFrame(tick);
+                }
+            }
+
+            MM.getFrame(tick);
+        };
+
+        return easey;
     };
 
     this.easey = easey;
