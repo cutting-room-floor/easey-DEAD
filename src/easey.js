9789608c8f067053eed62b44843b84384169450c
Ansis Brammanis <ansis.brammanis@gmail.com>
2012-07-13 16:44:31 -0400

    Fix zoom-only bug in optimal

--- src/easey.js
+++ src/easey.js
@@ -245,8 +245,8 @@
                 var s = t * S,
                     us = u(s),
                     z = a.zoom + (Math.log(w0/w(s)) / Math.LN2),
-                    x = interp(c0.x, c1.x, us/u1),
-                    y = interp(c0.y, c1.y, us/u1);
+                    x = interp(c0.x, c1.x, us/u1 || 1),
+                    y = interp(c0.y, c1.y, us/u1 || 1);
                 return new MM.Coordinate(y, x, 0).zoomTo(z);
             };
 
