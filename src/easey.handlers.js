014438aedb2390a18306e8cf4160ef5537877a3e
Ansis Brammanis <ansis.brammanis@gmail.com>
2012-11-02 18:29:47 -0400

    Re-add precise for mousewheel handler

--- src/easey.handlers.js
+++ src/easey.handlers.js
@@ -263,7 +263,6 @@
 
         function mouseWheel(e) {
             var delta = 0;
-            prevTime = prevTime || new Date().getTime();
 
             try {
                 _zoomDiv.scrollTop = 1000;
@@ -273,24 +272,31 @@
                 delta = e.wheelDelta || (-e.detail * 5);
             }
 
-            // limit mousewheeling to once every 200ms
-            var timeSince = new Date().getTime() - prevTime;
 
             function dispatchZoomed() {
                 map.dispatchCallback('zoomed');
             }
 
-            if (!ea.running()) {
-              var point = MM.getMousePoint(e, map),
-                  z = map.getZoom();
-              ea.map(map)
-                .easing('easeOut')
-                .to(map.pointCoordinate(MM.getMousePoint(e, map)).zoomTo(z + (delta > 0 ? 1 : -1)))
-                .path('about').run(100, dispatchZoomed);
-                prevTime = new Date().getTime();
-            } else if (timeSince > 150){
-                ea.zoom(ea.to().zoom + (delta > 0 ? 1 : -1)).from(map.coordinate).resetRun();
-                prevTime = new Date().getTime();
+            if (precise) {
+                map.zoomByAbout(delta > 0 ? 0.01 : -0.01, MM.getMousePoint(e, map));
+
+            } else {
+                // limit mousewheeling to once every 200ms
+                prevTime = prevTime || new Date().getTime();
+                var timeSince = new Date().getTime() - prevTime;
+
+                if (!ea.running()) {
+                  var point = MM.getMousePoint(e, map),
+                      z = map.getZoom();
+                  ea.map(map)
+                    .easing('easeOut')
+                    .to(map.pointCoordinate(MM.getMousePoint(e, map)).zoomTo(z + (delta > 0 ? 1 : -1)))
+                    .path('about').run(100, dispatchZoomed);
+                    prevTime = new Date().getTime();
+                } else if (timeSince > 150){
+                    ea.zoom(ea.to().zoom + (delta > 0 ? 1 : -1)).from(map.coordinate).resetRun();
+                    prevTime = new Date().getTime();
+                }
             }
 
             // Cancel the event so that the page doesn't scroll
