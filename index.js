5c586f3d5b6a2752627874638aa2a79c49aa06c1
Will White <will@mapbox.com>
2012-12-17 18:30:48 -0500

    Use example map instead of mapbox-streets.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var map;
 window.onload = function() {
     var mm = com.modestmaps;
     var dmap = document.getElementById('map');
-    wax.tilejson('http://a.tiles.mapbox.com/v3/mapbox.mapbox-streets.jsonp',
+    wax.tilejson('http://a.tiles.mapbox.com/v3/examples.map-vyofok3q.jsonp',
         function(tj) {
         map = new com.modestmaps.Map(dmap,
             new wax.mm.connector(tj), null, [
